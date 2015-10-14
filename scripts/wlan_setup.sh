fd5a419ee30d240cbac5244eb536c143dbf698d3
Morten Albeck Nielsen <morten.albeck-nielsen@leica-geosystems.com>
2015-10-14 16:13:24 +0200

    Script to put and release the wireless for monitor mode.

---
+++ scripts/wlan_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ "$1" == "m" ] || [ "$1" == "mon" ] || [ "$1" == "monitor" ]; then
+    # Set the wireless up for monitoring
+    sudo ifconfig wlan0 down
+    sudo stop network-manager 
+    sudo iwconfig wlan0 mode monitor
+    sudo ifconfig wlan0 up
+else
+    # Let the wireless be handled by the network-manager
+    sudo ifconfig wlan0 down
+    sudo start network-manager 
+    sudo iwconfig wlan0 mode managed
+    sudo ifconfig wlan0 up
+fi
