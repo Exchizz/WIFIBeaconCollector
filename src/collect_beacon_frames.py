0e7248e5b5f381a55e72cde16a9f4250f4a45636
Mathias Neerup <manee12@student.sdu.dk>
2017-04-26 10:49:19 +0200

    Output frequency and channel.
    Idea by Sara Marie Gadgaard

--- src/collect_beacon_frames.py
+++ src/collect_beacon_frames.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import socket 
+import socket
 import time
 
 lookForMac = ''
@@ -9,10 +9,13 @@ rawSocket.bind(("wlan0", 0x0003))
 
 count = 200
 
+# The following indices is found by inspection of wlan traffic in Wireshark.
 while count > 0:
-	pkt = rawSocket.recvfrom(2048)[0]
-	if pkt[18] == "\x80" :
-		essid = pkt[56:56 +ord(pkt[55])]
-		mac = pkt[28:34].encode('hex')
-		signal = 256-int(ord(pkt[14]))
-		print "Time: %s MAC: %s Signal: %d ESSID: %s" % (time.strftime('%H:%M:%S'),mac, signal, essid)
+        pkt = rawSocket.recvfrom(2048)[0]
+        if pkt[18] == "\x80" :
+                essid = pkt[56:56 + ord(pkt[55])]
+                mac = pkt[28:34].encode('hex')
+                signal = 256-int(ord(pkt[14]))
+                freq = int(pkt[11].encode('hex') + pkt[10].encode('hex'),16)
+                channel = 1 + (freq-2412) / 5 # substract 2412(channel 1), divide by 5 since it's the distance between center freqs
+                print "Time: %s MAC: %s Signal: %d ESSID: %s, Freq: %d, Channel: %d" % (time.strftime('%H:%M:%S'),mac, signal, essid, freq, channel)
